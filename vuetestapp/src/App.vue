c074f0ab4cc9cbac24085c3d78f8afdc6e37ff99
Никита <97441578+JabkaScript@users.noreply.github.com>
2023-05-03 13:06:55 +0300

    Initial commit

---
+++ vuetestapp/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <p>Здарова, {{username}}</p>
+    <input @change="setUserName" v-bind:value="username"/>
+    <button @click="setUserName">SetName</button>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            username:"JabkaScript",
+            
+        }
+        
+    },
+    methods: {
+        setUserName(e){
+            this.username = e.target.value;
+        }
+    }    
+}
+</script>
+
+<style></style>
